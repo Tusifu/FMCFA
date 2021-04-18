3d149c051b83a1e039255862e0179d0732903345
Tusifu Edison <yotusifuedison@gmail.com>
2021-04-18 16:28:10 +0200

    implement pharmacist

---
+++ workers/urls/pharmacist_urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from workers.views import pharmacistViews
+
+urlpatterns = [
+    path('create', pharmacistViews.PharmacistCreateView.as_view(), name='create_pharmacist'),
+    path('list', pharmacistViews.PharmacistList.as_view(), name='pharmacist_list'),
+    path('<str:id>/edit',pharmacistViews.PharmacistUpdate.as_view(),name="pharmacist_edit"),
+    path('<str:id>/delete',pharmacistViews.delete_pharmacist,name="pharmacist_delete"),
+    path('<str:id>/detail',pharmacistViews.pharmacist_detail,name="pharmacist_detail"),
+
+]
+
