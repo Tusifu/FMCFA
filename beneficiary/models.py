9a11ce8f39e43d58ea7532edb6e9c3c85ca21bbb
anonelson <ishimwenelson45@gmail.com>
2021-04-25 14:47:17 +0200

    refactoring code and adding search

--- beneficiary/models.py
+++ beneficiary/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django import forms
+from django.forms import ModelForm
+from django.forms import Select, TextInput, Textarea
 # Create your models here.
 class Beneficiary(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
@@ -16,3 +19,20 @@ class Beneficiary(models.Model):
 
     class Meta:
         db_table = "Beneficiary"
+
+class BeneficiaryForm(forms.ModelForm):
+
+    class Meta:
+        model = Beneficiary
+        fields = '__all__'
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control', 'placeholder': "Enter beneficiary"}),
+            'address': TextInput(attrs={'class': 'form-control', 'placeholder': "Enter your Address"}),
+            'age': TextInput(attrs={'class': 'form-control', 'placeholder': "Enter your age"}),
+            'province': TextInput(attrs={'class': 'form-control', 'placeholder': "Enter your province"}),
+            'dob': TextInput(attrs={'class': 'form-control', 'type':'date'}),
+            'district': TextInput(attrs={'class': 'form-control', 'placeholder': "Enter your district"}),
+            'sector': TextInput(attrs={'class': 'form-control', 'placeholder': "Enter your sector"}),
+            'cell': TextInput(attrs={'class': 'form-control', 'placeholder': "Enter your cell"}),
+            
+        }
